5d7ad4c54b3f62fe8ee62dfdcc2c1b6e2e31bc74
mike connor <mike@rocketnumber9.org>
2020-03-25 15:48:20 -0500

    api routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,48 @@
-import { NextPage } from 'next';
-import Layout from '../components/Layout';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
-
-type PostType = {
-    id:string,
-    title: string
-}
-type Props = {
-    post: PostType
+function fetcher(url:string) {
+  return fetch(url).then(r => r.json());
 }
 
-function getPosts():Array<PostType> {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js' },
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-  ];
-}
-
-
-
-const PostLink:NextPage<Props> = ({ post }) => (
-    <li>
-      <Link href="/p/[id]" as={`/p/${post.id}`}>
-        <a>{post.title}</a>
-      </Link>
-      <style jsx>{`
-         
-          a {
-            font-family: 'Arial';
-          }
-  
-          ul {
-            padding: 0;
-          }
-  
-          li {
-            list-style: none;
-            margin: 5px 0;
-          }
-  
-          a {
-            text-decoration: none;
-            color: blue;
-          }
-  
-          a:hover {
-            opacity: 0.6;
-          }
-        `}</style>
-    </li>
+export default function Index() {
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher
   );
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.author`
+  const author = data?.author;
+  let quote = data?.quote;
 
-const Blog = ()=> {
-    return (
-      <Layout>
-        <h1>My Blog</h1>
-        <ul>
-          {getPosts().map(post => (
-            <PostLink key={post.id} post={post} />
-          ))}
-        </ul>
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
 
-      </Layout>
-    );
-  }
-  
-  export default Blog;
+  return (
+    <main className="center">
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
+
+      <style jsx>{`
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
+        }
+        .quote {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
+        }
+        .author {
+          font-family: sans-serif;
+          color: #559834;
+          font-size: 20px;
+        }
+      `}</style>
+    </main>
+  );
+}
