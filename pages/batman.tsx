d42f566418d087658e95080fe54f2e265a408966
mike connor <mike@rocketnumber9.org>
2020-03-25 14:51:11 -0500

    move page

---
+++ pages/batman.tsx
@@ -0,0 +1,54 @@
+import { NextPage } from 'next'
+import Layout from '../components/Layout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+
+type ShowType = {
+    id:string,
+    name:string
+}
+
+type Props = {
+    shows: Array<ShowType>;
+}
+
+type ScoreShowType = {
+    score: number,
+    show: ShowType
+}
+
+const Batman:NextPage<Props> = ({ shows }) => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+
+
+
+
+//https://nextjs.org/docs/api-reference/data-fetching/getInitialProps
+// If you're using Next.js 9.3 or newer, we recommend that
+//  you use getStaticProps or getServerSideProps instead of getInitialProps.
+
+Batman.getInitialProps= async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data:Array<ScoreShowType> = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Batman;
